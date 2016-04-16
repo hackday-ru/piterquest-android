6588e1d2505342b5360b68898f8c93ed14c50d94
Mikelle <mikhwall@gmail.com>
2016-04-16 15:09:53 +0300

    Added simple listView

--- app/src/main/java/com/piterquest/activity/ListQuestActivity.java
+++ app/src/main/java/com/piterquest/activity/ListQuestActivity.java
@@ -2,11 +2,16 @@ package com.piterquest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.piterquest.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ListQuestActivity extends AppCompatActivity {
 
@@ -17,8 +22,23 @@ public class ListQuestActivity extends AppCompatActivity {
 
         final ListView questListView = (ListView) findViewById(R.id.quest_list);
 
-        String[] quests = new String[]{};
+        String[] quests = new String[]{"Quest 1", "Quest 2", "Quest 3"};
         final ArrayList<String> questList = new ArrayList<>();
+        Collections.addAll(questList, quests);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, quests);
+        assert questListView != null;
+        questListView.setAdapter(adapter);
+
+        questListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) questListView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), "Quest : " + itemValue + " was selected",
+                        Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
 
     }
 }
