318dba334ad56bb5d35e5fdda7f1f312804695de
Mikelle <mikhwall@gmail.com>
2016-04-16 14:02:40 +0300

    Fixed .gradle

--- app/src/main/java/com/piterquest/activity/ListQuestActivity.java
+++ app/src/main/java/com/piterquest/activity/ListQuestActivity.java
@@ -1,7 +1,9 @@
-package com.piterquest;
+package com.piterquest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.piterquest.R;
 
 public class ListQuestActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class ListQuestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_quest);
+
+
     }
 }
