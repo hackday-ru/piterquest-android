5a71e59b5cb23560dc02ff447fc66d26ff2f0cc5
Mikelle <mikhwall@gmail.com>
2016-04-16 13:53:23 +0300

    Init commit

---
+++ app/src/main/java/com/piterquest/ListQuestActivity.java
@@ -0,0 +1,13 @@
+package com.piterquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListQuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_quest);
+    }
+}
