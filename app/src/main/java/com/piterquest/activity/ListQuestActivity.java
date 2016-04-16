022f009276390b3e038728cf26b3597be47bea4f
Mikelle <mikhwall@gmail.com>
2016-04-16 14:39:44 +0300

    Adding to epam repo

--- app/src/main/java/com/piterquest/activity/ListQuestActivity.java
+++ app/src/main/java/com/piterquest/activity/ListQuestActivity.java
@@ -2,8 +2,11 @@ package com.piterquest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.piterquest.R;
+
+import java.util.ArrayList;
 
 public class ListQuestActivity extends AppCompatActivity {
 
@@ -12,6 +15,10 @@ public class ListQuestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_quest);
 
+        final ListView questListView = (ListView) findViewById(R.id.quest_list);
+
+        String[] quests = new String[]{};
+        final ArrayList<String> questList = new ArrayList<>();
 
     }
 }
