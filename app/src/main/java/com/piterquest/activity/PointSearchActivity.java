1cda2f6ec5b3cbbb100545f5b2142fe768a48bec
ksmirenko <k.smirenko@gmail.com>
2016-04-16 18:17:08 +0300

    Added design for PointSearchActivity

---
+++ app/src/main/java/com/piterquest/activity/PointSearchActivity.java
@@ -0,0 +1,15 @@
+package com.piterquest.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.piterquest.R;
+
+public class PointSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_search);
+    }
+}
